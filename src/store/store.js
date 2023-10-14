51e601a59e22ce51d89dd2bd9823da8e1bea72ac
Florece Ann Robles <annr.dev@gmail.com>
2023-10-13 21:35:05 -0400

    Updates

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { observable } from "mobx";
-import { RouterStore } from "@ibm/mobx-react-router";
-
-class AppStore {
-	@observable title = "ReactJS Tasks";
-}
-
-export const appStore = new AppStore();
-export const routerStore = new RouterStore();
